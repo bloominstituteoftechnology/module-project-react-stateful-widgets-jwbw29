0988a219f4bfba65fea9d25bbd68faffb847f49b
Justin Byrd <justinbyrd7@gmail.com>
2023-07-27 14:57:59 -0500

    complete

--- frontend/index.js
+++ frontend/index.js
@@ -1,24 +1,24 @@
-import React from 'react'
-import { createRoot } from 'react-dom/client'
-import './styles/reset.css'
-import './styles/styles.css'
-import Counter from './components/Counter';
-import Input from './components/Input';
-import Moods from './components/Moods';
-import Spinner from './components/Spinner';
-import Squares from './components/Squares';
-import Programmers from './components/Programmers';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import "./styles/reset.css";
+import "./styles/styles.css";
+import Counter from "./components/Counter";
+import Input from "./components/Input";
+import Moods from "./components/Moods";
+import Spinner from "./components/Spinner";
+import Squares from "./components/Squares";
+import Programmers from "./components/Programmers";
 
-const domNode = document.getElementById('root')
-const root = createRoot(domNode)
+const domNode = document.getElementById("root");
+const root = createRoot(domNode);
 
 root.render(
   <>
-    {/* <Counter />
+    <Counter />
     <Moods />
     <Spinner />
-    <Input /> */}
+    <Input />
     <Squares />
-    {/* <Programmers /> */}
+    <Programmers />
   </>
-)
+);
